1c7a80a0c78d1bc14617ae10e7795f7f9cf88279
Francis <francisaldrick444@gmail.com>
2020-02-14 21:39:45 +0530

    front end final

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'library#index'
-  get '/home', to:'library#home'
+  get '/home', to:'library#index'
   get '/login', to:'library#librarian'
   get '/addbook', to:'library#books_details'
   get '/addstudent', to:'library#student_details'
-  get '/addchoose', to:'library#choose'
+  get '/addchoose', to:'library#new'
   get '/dispbook', to:'library#displaybook'
   get '/stdnt', to:'library#student'
+
+  resources :library
 
 end
