e8177caadc2b1c5dbd2a80010f7b0c0f56b2c4b3
Francis <francisaldrick444@gmail.com>
2020-02-17 22:30:04 +0530

    added changes

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_02_17_061707) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookdetails", force: :cascade do |t|
     t.string "ISBN"
     t.string "Book"
